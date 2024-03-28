373de4fce76366883099f351990f361deba84643
hardcoreseb <sebastian.reinhold@yahoo.de>
2024-03-28 13:45:28 +0100

    Icons & Logos

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from youtube_api import validate_youtube_link, extraxt_playlist_id, retrieve_playlist_elements
+from PIL import Image
 
 import customtkinter
 
@@ -6,22 +7,33 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 class AppGUI:
-    width = 600
-    height = 400
+    width = 650
+    height = 500
 
     def __init__(self) -> None:
         self.root = customtkinter.CTk()
         self.root.geometry(f"{self.width}x{self.height}")
         self.root.resizable(False, False)
         self.root.title("Youtube2Spotify")
+        self.root.iconbitmap(r"assets/ytb2sptfy.ico")
+
+        default_font = customtkinter.CTkFont(family="system-ui", size=16)
+        logo_image = customtkinter.CTkImage(
+            light_image=Image.open('assets/yt2sptfy_nobg.png'), 
+            dark_image=Image.open('assets/yt2sptfy_nobg.png'),
+            size=(100,100)
+            )
         
         self.frame = customtkinter.CTkFrame(master=self.root)
         self.frame.pack(pady=24, padx=40, fill="both", expand=True)
 
-        self.label = customtkinter.CTkLabel(master=self.frame, text="Youtube2Spotify Converter", font=customtkinter.CTkFont(family="system-ui, Roboto", size=24))
-        self.label.pack(pady=40, padx=16)
+        self.logo_image_label = customtkinter.CTkLabel(master=self.frame, text="", image=logo_image)
+        self.logo_image_label.pack(pady=(0,0), padx=16)
 
-        self.entryLabel = customtkinter.CTkLabel(master=self.frame, text="Please insert a full YouTube playlist link", font=customtkinter.CTkFont(family="system-ui, Roboto", size=16))
+        self.label = customtkinter.CTkLabel(master=self.frame, text="Youtube2Spotify Converter", font=customtkinter.CTkFont(family="system-ui", size=24))
+        self.label.pack(pady=(8,32), padx=16)
+
+        self.entryLabel = customtkinter.CTkLabel(master=self.frame, text="Please insert a full YouTube playlist link", font=default_font)
         self.entryLabel.pack(pady=(24,8), padx=16)
 
         self.entry = customtkinter.CTkEntry(master=self.frame, placeholder_text="example: https://www.youtube.com/playlist?list=PLy6N_9yB8Qwy6LL0J7zLUyW8XNX-BPeDl", width=400, height=40)
@@ -29,10 +41,10 @@ class AppGUI:
         self.entry.bind("<Return>", self.check)
 
 
-        self.button = customtkinter.CTkButton(master=self.frame, text="Get all Playlist Elements", command=self.check)
-        self.button.pack(pady=(16,40), padx=24, side="bottom")
+        self.button = customtkinter.CTkButton(master=self.frame, text="Get all Playlist Elements", command=self.check, font=default_font)
+        self.button.pack(pady=(16,40), padx=24, ipady=4, ipadx=4, side="bottom")
 
-        self.labelDone = customtkinter.CTkLabel(master=self.frame, text="Done!", font=customtkinter.CTkFont(family="system-ui, Roboto", size=16))
+        self.labelDone = customtkinter.CTkLabel(master=self.frame, text="Done!", font=default_font)
         
 
     def check(self, event = None):
